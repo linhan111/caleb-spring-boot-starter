eb1455b986d4d0e71a31ffa90c2156b134ebc3d8
calebl <calebl@synnex.com>
2019-07-31 15:20:58 +0800

    add feature：项目通用线程池配置（粉笔针对cpu与io密集型任务）

---
+++ src/main/java/com/demo/utils/ThreadPoolConfig.java
@@ -0,0 +1,47 @@
+package com.demo.utils;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * CPU intensive tasks and IO intensive tasks Thread pool config
+ *
+ * @author lhan111
+ */
+@Configuration
+public class ThreadPoolConfig {
+    // google ThreadFactory config
+    /*final ThreadFactory threadFactory = new ThreadFactoryBuilder()
+            .setNameFormat("cpu-async-task-name-%d")
+            .setDaemon(true)
+            .build();*/
+
+    @Bean("cpuTaskExecutor")
+    public Executor cpuTaskExecutor() {
+        return new ThreadPoolExecutor(Runtime.getRuntime().availableProcessors(),
+                2 * Runtime.getRuntime().availableProcessors(),
+                5, TimeUnit.SECONDS,
+                new LinkedBlockingQueue<>(256),
+                r -> new Thread(r, "cpu-async-task-name-%d"), (r, executor) -> {
+            // 打印日志,添加监控等
+            System.out.println("task is rejected!");
+        });
+    }
+
+    @Bean("ioTaskExecutor")
+    public Executor ioTaskExecutor() {
+        return new ThreadPoolExecutor(2 * Runtime.getRuntime().availableProcessors(),
+                3 * Runtime.getRuntime().availableProcessors(),
+                5, TimeUnit.SECONDS,
+                new LinkedBlockingQueue<>(256),
+                r -> new Thread(r, "io-async-task-name-%d"), (r, executor) -> {
+            // 打印日志,添加监控等
+            System.out.println("task is rejected!");
+        });
+    }
+}
